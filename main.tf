3e2cdb1e4b91f9047658f61542a78cb0da377caf
Nagiri Navya <navya.kongala@kyndryl.com>
2025-08-12 17:27:57 +0530

    seventh commit

--- main.tf
+++ main.tf
@@ -1,76 +1,90 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
-features {}
-subscription_id = var.subscription_id
+  features {}
 }
- 
-# Use existing resource group
+
 data "azurerm_resource_group" "existing" {
-name = var.resource_group_name
-#location = "East US"
+  name = "rg-cp-navya-kongala"
 }
-# Virtual network 
+
 resource "azurerm_virtual_network" "vnet" {
-name = "windows-vnetfinal2"
-address_space = ["10.0.0.0/16"]
-location = data.azurerm_resource_group.existing.location
-resource_group_name = data.azurerm_resource_group.existing.name
+  name                = "windows-vnetfinal2"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+
+  tags = {
+    Environment = "Dev"
+    Owner       = "Navya"
+    Department  = "delivery"
+  }
 }
- 
-# Subnet
+
 resource "azurerm_subnet" "subnet" {
-name = "windowssubnetfinal2"
-resource_group_name = data.azurerm_resource_group.existing.name
-virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = ["10.0.2.0/24"]
+  name                 = "windows-subnetfinal2"
+  resource_group_name  = data.azurerm_resource_group.existing.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  tags = {
+    Environment = "Dev"
+    Owner       = "Navya"
+    Department  = "delivery"
+  }
 }
-# Generate public ip
-/*resource "azurerm_public_ip" "public_ip" {
-name = "v-ip"
-location = data.azurerm_resource_group.existing.location
-resource_group_name = data.azurerm_resource_group.existing.name
-allocation_method = "Static"
-}*/
- 
-# Network interface
+
 resource "azurerm_network_interface" "nic" {
-name = "windows-nicfinal2"
-location = data.azurerm_resource_group.existing.location
-resource_group_name = data.azurerm_resource_group.existing.name
- 
-ip_configuration {
-name = "internal"
-subnet_id = azurerm_subnet.subnet.id
-private_ip_address_allocation = "Dynamic"
-#public_ip_address_id = azurerm_public_ip.public_ip.id
+  name                = "windows-nicfinal2"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    Environment = "Dev"
+    Owner       = "Navya"
+    Department  = "delivery"
+  }
 }
-}
- 
-# VM creation
+
 resource "azurerm_windows_virtual_machine" "vm" {
-name = "myWindowsVMex2"
-resource_group_name = data.azurerm_resource_group.existing.name
-location = data.azurerm_resource_group.existing.location
-size = "Standard_D2s_v3"
-admin_username = "azureuser"
-admin_password = "azureuser@1234" # Use a secure password
-network_interface_ids = [
-azurerm_network_interface.nic.id,
-]
- 
-os_disk {
-caching = "ReadWrite"
-storage_account_type = "Standard_LRS"
-}
- 
-source_image_reference {
-publisher = "MicrosoftWindowsServer"
-offer = "WindowsServer"
-sku = "2019-datacenter" 
-version = "latest"
-}
-# tags
-tags = {
-Environment = "Dev"
-Owner = "Navya"
-}
+  name                = "windows-vmfinal2"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+  admin_password      = "P@ssw0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  tags = {
+    Environment = "Dev"
+    Owner       = "Navya"
+    Department  = "delivery"
+  }
 }
